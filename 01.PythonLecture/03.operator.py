2f5789fb30a736b725523ac6d523984d91e30fac
Yeo, Dongbin <yeodongbin2011@gmail.com>
2020-12-17 16:13:06 +0900

    Update 03.operator.py

--- 01.PythonLecture/03.operator.py
+++ 01.PythonLecture/03.operator.py
@@ -29,58 +29,119 @@ and (불리언 AND 연산자)
 or (불리언 OR 연산자)
 
 '''
+#연산자 * ** / + - // %
+a = 10
+b = 3
+#사칙연산
+print(a+b)   #13
+print(a-b)   #7
+print(a/b)   #3.33333..
+print(a*b)   #30
+print(a**b)  #10^3 = 10*10*10 = 1000
+print(a//b)  #3
+print(a%b)   #1
 
-a = 2020
-b = 1
-c = 3
-print(a,b,c, sep='/', end = ' ')
-print(a,b,c)
+#a % 2 == 0 =>2의 배수
 
-d = 10
-e = '10'
-print (10 + int(e))
-print (str(d) + e)
+#비교연산자
+#관계 연산자 
+# ==(같다) !=(같지 않다)  >  <   >=   <=  => True or False
+a = 100; b =200
+print(a==b, a!=b, a > b, a<b, a>=b, a<=b) # F T F T F T
+print(a > b)   #T
+print(a >= b)  #T
+print(a < b)   #F
+print(a <= b)  #F
+print(a == b)  #F
+print(a != b)  #T
 
-f = True
-g = False
-print(type(a))
+#논리연산자 (and or not )  T and F ==> F
+# A and B  ,  A or B , not A
+# and
+print(False and False) #F
+print(True and False)  #F
+print(False and True)  #F
+print(True and True)   #T
 
-print(bool(' '))
-print(bool(''))
-print(bool(0))
-print(bool(1))
-print(bool(-1))
-print(bool())
-print(bool(None))
+# or
+print(False or False) #F
+print("F or F = ",(a==b)or(a==101)) #F or F = F
+print(True or False)  #T
+print(False or True)  #T
+print(True or True)   #T
 
-h = 3
-j = 10
-print(h + j)
-print(h - j )
-print(j / h ) #float
-print(j // h ) #int
-print(h * j )
-print(h ** j )
-print(h % j )
+# not
+print(not(True))      #F
+print("not(a==100) = ", not(a == 100))  
+print("not(a==100) = %s" %(not(a==100))) #T -> F
+print("not(a==100) = {}".format(not(a==100))) #T -> F
 
-#비교 연산
-print(h >= j)
-print(h > j )
-print(j < h )
-print(j <= h )
-print(h == j )
-print(h !=  j )
+c = True
+d = False
+print( c and d)   # F
+print( c or d)    # T
+print( not d)     # T
+print((a > b) and (a!=b )) # T and T => T
+print(bool(1) and bool(0)) # F
+print((a>=b) and (not bool(None))) #T
 
-#논리 연산
-print(f and g)
-print(f or g)
-print(not f)
 
 #할당 연산
-k =10
-k = k +10
-k += 10
-print(k)
+e = 10
+e += 10 #e = e + 10  => 20
+e -= 10 #e = e - 10  => 10
+e *= 10 #e = e * 10  => 100
+e /= 10 #e = e / 10  => 10.0
+
+print(e)
+
+f = 10
+f += 2 # 12
+f -= 2 # 10
+f *= 2 # 20
+f /= 2 # 10.0
+f %= 2 # 0
+f //= 2 # 0
+f **= 2 # 0
+
+print(f)
+
+# -------------------------------------
+#시프트 연산  <<  >>  
+a = 1
+print(a<<2) # 4  001(2) -> 100(2)
+print(a>>1) # 0  001(2) -> 000(2)
+print(a>>3) # 0  001(2) -> 000(2)
+
+g = 2    # 0b0010 2
+print(g)
+g = g<<2 # 0b1000 8
+print(g)
+g = g>>1 # 0b0100 4
+print(g)
+
+#비트논리연산 &(and)  |(or) ^(xor) ~(not)
+a = 5; b = 3 # 0101 & 0011 = 0001
+print(a & b) # 1
+print(a | b) # 7
+print(0b0101 & 0b0011)
+print(~5)
+print(~0b0101)
+print(5^3)   # 6
+
+h = 5 # 0b 0101
+i = 3 # 0b 0011
+
+print(h&i) #?  0001
+# 0b 0101
+# 0b 0011
+#    0001
+print(h|i) #?  0111
+# 0b 0101
+# 0b 0011
+#    0111 
+print(h^i) #?  0110
+print(~(-1)) #?
 
 #bit 연산
 a = 40
@@ -94,34 +155,7 @@ print(bin(b)[2:].zfill(6))
 # ------ And 연산
 # 001000
 
-########################################
-
-# 산술연산자 + - *  **  /(나눗셈)  //  %  
-a = 11 #int 정수
-b = 4  #int 
-print(a + b)
-print(a - b)
-print(a * b)
-print(a ** b) # 11^4 = 11 * 11 * 11 * 11
-print(a / b)  # 2.75
-print(a // b) # 2 몫
-print(a % b)  # 3 나머지
-
-# 20 % 2
-print(20 % 2 ) # if (0) = > 짝수, if (1) => 홀수
-# 20 ? 3의 배수
-print(20 % 3 ) # if (0) = > 3의 배수, if (1 or 2) => 3의배수 X
-
-# 대입연산자 
-a = 10
-a += 2; print(a)   # a = a + 2   12
-a -= 2; print(a)   # a = a - 2   10
-a *= 2; print(a)   # a = a * 2   20
-a /= 2; print(a)  # a = a / 2    10.0
-a //= 2; print(a)  # a = a // 2  5.0
-a %= 2; print(a)   # a = a % 2   1.0
-a **= 2; print(a) # a = a^2      1.0
-
+#######################################
 # 문제) 15500 => 화폐 10000 5000 1000 500 100 50 10
 # 답 : 10000 1장, 5000 1장, 500 1개
 money = 15500
@@ -148,48 +182,3 @@ print("만원권   : {} 장 \n".format(m10000) +
       "50원    : {} 개\n".format(m50) +
       "10원    : {} 개".format(m10))
 
-# 관계 연산자 
-# ==(같다) !=(같지 않다)  >  <   >=   <=  => True or False
-a = 100; b =200
-print(a==b, a!=b, a > b, a<b, a>=b, a<=b) # F T F T F T
-
-# 논리 연산자
-# A and B  ,  A or B , not A
-# and
-print(False and False) #F
-print(True and False)  #F
-print(False and True)  #F
-print(True and True)   #T
-
-# or
-print(False or False) #F
-print("F or F = ",(a==b)or(a==101)) #F or F = F
-print(True or False)  #T
-print(False or True)  #T
-print(True or True)   #T
-
-# not
-print(not(True))      #F
-print("not(a==100) = ", not(a == 100))  
-print("not(a==100) = %s" %(not(a==100))) #T -> F
-print("not(a==100) = {}".format(not(a==100))) #T -> F
-
-# bit 연산
-# & and
-# | or
-# ~ not
-# ^ xor
-a = 5; b = 3 # 0101 & 0011 = 0001
-print(a & b) # 1
-print(a | b) # 7
-print(0b0101 & 0b0011)
-print(~5)
-print(~0b0101)
-print(5^3)   # 6
-
-# shift 연산
-# <<  >>
-a = 1
-print(a<<2) # 4  001(2) -> 100(2)
-print(a>>1) # 0  001(2) -> 000(2)
-print(a>>3) # 0  001(2) -> 000(2)
