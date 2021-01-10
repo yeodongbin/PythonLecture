1e49f69e672d5bf3502f79704a52590ee47b99a5
Yeo, Dongbin <yeodongbin2011@gmail.com>
2021-01-11 00:16:11 +0900

    Create tree.py
    create binery tree searching

---
+++ 02.DataStructure/tree.py
@@ -0,0 +1,13 @@
+#Root node
+#parent node
+#child node
+#siblings(brother node)
+#leaf node(terminal node)
+
+class Node:
+  def __init__(self, value):
+    self.value = value
+    self.left, self.right = None, None
+    
+
+    
