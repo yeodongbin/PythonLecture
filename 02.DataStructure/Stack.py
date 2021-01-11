7d10b5428f65a16c2ef8f05d0d6b2a6539eab125
Yeo, Dongbin <yeodongbin2011@gmail.com>
2021-01-11 10:15:46 +0900

    Update Stack.py

--- 02.DataStructure/Stack.py
+++ 02.DataStructure/Stack.py
@@ -1,31 +1,80 @@
 # Stack 클래스 정의 - python list 활용
-class Stack(list):
+class Stack():
+    class Node():
+        def __init__(self):
+            self.__data = None
+
+        def input(self,data):
+            self.__data = data
+
+        def output(self):
+            return self.__data
+
     def __init__(self):
         self.stack = []
 
-    def is_empty(self):   # 데이터가 없는지 확인
-        if len(self.stack) == 0:
-            return True
-        else:
-            return False
-
     def push(self, data):
-        self.stack.append(data)
-
+        if (self.count()<4):
+            self.node = self.Node()
+            self.node.input(data)
+            self.stack.append(self.node)
+        else :
+            print("stack is full~~~~ : {}".format(self.count()))
+    
     def pop(self):
-        if self.is_empty():
-            return -1
-        return self.stack.pop()
+        node = self.stack.pop(len(self.stack)-1)
+        return node.output()
 
-    def peek(self):        # 최상단 데이터 확인
-        return self[-1]
+    def count(self):
+        count = len(self.stack)
+        return count
 
-if __name__=="__main__":
+    def print(self):
+        print("[",end='')
+        for node in self.stack:
+            print(node.output(), end=",")
+        print("]")
+        
+    
+
+if __name__ == '__main__':
     s = Stack()
-    s.push(1)
-    s.push(2)
-    s.push(3)
+    s.push('banana')
+    s.push('apple')
+    print(s.count())
+    s.print()
 
-    while s:
-        data = s.pop()
-        print(data, end=' ') # 3 2 1
+    print(s.pop())
+    s.count()
+    s.print()
+    
+    
+#  def __init__(self):
+#         self.stack = []
+
+#     def is_empty(self):   # 데이터가 없는지 확인
+#         if len(self.stack) == 0:
+#             return True
+#         else:
+#             return False
+
+#     def push(self, data):
+#         self.stack.append(data)
+
+#     def pop(self):
+#         if self.is_empty():
+#             return -1
+#         return self.stack.pop()
+
+#     def peek(self):        # 최상단 데이터 확인
+#         return self[-1]
+
+# if __name__=="__main__":
+#     s = Stack()
+#     s.push(1)
+#     s.push(2)
+#     s.push(3)
+
+#     while s:
+#         data = s.pop()
+#         print(data, end=' ') # 3 2 1
