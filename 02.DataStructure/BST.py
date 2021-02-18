23f671efee27c15c47ecf38cc980ddcef2d7df80
Dongbin Yeo <yeodongbin2011@gmail.com>
2021-02-18 17:48:40 +0900

    update

--- 02.DataStructure/BST.py
+++ 02.DataStructure/BST.py
@@ -1,19 +1,3 @@
-def factorial_01(input):
-    n = input + 1
-    result = 1
-    for i in range(1,n,1):
-        result *= i
-    return result
-
-# 재귀함수
-def factorial_02(input):
-    if (input > 1):
-        return input * factorial_02(input-1)
-    else:
-        return 1
-
-print(factorial_01(5))
-print(factorial_02(5))
 
 #Root node
 #Parent node
